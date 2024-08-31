107b1265d5b3cb2e40225f3406d9fbbc2db637d4
rogercf17 <rogercf16@gmail.com>
2024-08-30 21:43:31 -0300

    Componente Header criado e implementado

---
+++ src/Componentes/Header/index.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return(
+        <header>
+            <ul>
+                <li>
+                    <p>Home</p>
+                </li>
+                <li>
+                    <p>Sobre</p>
+                </li>
+                <li>
+                    <p>Contato</p>
+                </li>
+            </ul>
+        </header>
+    );
+}
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+// import Sobre from './Pages/Sobre'
+// import Contato from './Pages/Contato'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App/>,
+//     children: [
+//       {path:'sobre', element: <Sobre/>},
+//       {path:'contato', element: <Contato/>},
+//     ]
+
+//   }
+// ])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <RouterProvider router={router} /> */}
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
