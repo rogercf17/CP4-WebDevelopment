d4e024e130b28538d05794003789e183696737cc
rogercf17 <rogercf16@gmail.com>
2024-09-02 14:32:31 -0300

    Componente hero criado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './Componentes/Header'
+import Hero from './Componentes/Hero'
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Home</h1>
+      {/* <h1>Home</h1> */}
+      <Hero />
     </>
   )
 }
---
+++ src/Componentes/Hero/index.jsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+    return(
+        <>
+            <section id="hero">
+                <div>
+                    <h1>Crie seus vídeos online</h1>
+                    <p>
+                        Transforme suas ideias em filmes memoráveis: 
+                        onde a criatividade encontra a simplicidade.
+                    </p>
+                    <button>Começar Agora</button>
+                </div>
+                <img src="" alt="" />
+            </section>
+        </>
+    )
+}
