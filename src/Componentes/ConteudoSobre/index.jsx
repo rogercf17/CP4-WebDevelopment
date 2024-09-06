f369dcce81e150c725dfd9f2b6363b954f862fa2
gabriel.lacerda <gabriel.lacerda@NO-20-02-V3481.micr.com>
2024-09-06 19:45:47 -0300

    Correção icone dropdown planos

--- src/Componentes/ConteudoSobre/index.jsx
+++ src/Componentes/ConteudoSobre/index.jsx
@@ -1,12 +1,12 @@
 export default function ConteudoSobre() {
-    return(
+    return (
         <>
             <section id='hero-dois'>
                 <div className="div-hero">
                     <h1>Bem-vindo à revolução dos vídeos!</h1>
                     <p>
-                        Somos uma startup com o objetivo de facilitar a 
-                        produção de conteúdo em larga escala com qualidade profissional, 
+                        Somos uma startup com o objetivo de facilitar a
+                        produção de conteúdo em larga escala com qualidade profissional,
                         capaz de atingir uma audiência de milhões de seguidores.
                     </p>
                     <button className="button-hero">Baixe o app</button>
@@ -24,10 +24,8 @@ export default function ConteudoSobre() {
                             <p>1 Usuário</p>
                             <div>
                                 <p>10 Vídeos <strong>R$15</strong></p>
-                                <img src="/icon-seta" alt="Icone" height="15px"/>
+                                <img src="/icon-seta.png" alt="Icone" height="15px" />
                             </div>
-                            <img src="/icon-seta" alt="Icone" height="15px" width="15px" className="icon"/>
-                            
                         </div>
                         <button className="btn-plano">Cadastrar</button>
                     </div>
@@ -38,15 +36,14 @@ export default function ConteudoSobre() {
                             <p>1-10 Usuários</p>
                             <div>
                                 <p>Vídeos Ilimitados <strong>R$40</strong></p>
-                                <img src="/icon-seta" alt="Icone" height="15px"/>
+                                <img src="/icon-seta.png" alt="Icone" height="15px" />
                             </div>
                             <p>+10 Usuários</p>
                             <div>
                                 <p>Vídeos Ilimitados <strong>R$20</strong></p>
-                                <img src="/icon-seta" alt="Icone" height="15px"/>
+                                <img src="/icon-seta.png" alt="Icone" height="15px" />
                             </div>
-                            <img src="/icon-seta" alt="Icone" height="15px" width="15px" className="icon"/>
-                            
+
                         </div>
                         <button className="btn-plano">Cadastrar</button>
                     </div>
@@ -54,8 +51,8 @@ export default function ConteudoSobre() {
                     <div className="plano corporativo">
                         <div className="titulo"><h3>Corporativo</h3></div>
                         <div className="informacoes">
-                            <img src="/icon-celular.png" height="110px" width="120px" alt="Imagem plano corporativo" className="icon-celular"/>
-                            
+                            <img src="/icon-celular.png" height="110px" width="120px" alt="Imagem plano corporativo" className="icon-celular" />
+
                         </div>
                         <button className="btn-plano">Entre Em Contato</button>
                     </div>
