96c7fe3165f3172cf5170e2ca3396e98d7fbd2ba
gabriel.lacerda <gabriel.lacerda@NO-20-02-V3481.micr.com>
2024-09-06 18:59:47 -0300

    Inserção de cards de redes socias na home page, atualização layout de planos

--- src/Componentes/ConteudoPrincipal/index.jsx
+++ src/Componentes/ConteudoPrincipal/index.jsx
@@ -1,16 +1,21 @@
+import Social from "../Social/Social";
+
 export default function ConteudoPrincipal() {
-    return(
+    return (
         <>
             <section id="hero">
                 <div className="div-hero">
                     <h1>Crie seus vídeos online</h1>
                     <p>
-                        Transforme suas ideias em filmes memoráveis: 
+                        Transforme suas ideias em filmes memoráveis:
                         onde a criatividade<br /> encontra a simplicidade.
                     </p>
                     <button className="button-hero">Começar Agora</button>
                 </div>
-                <img src="/Hero-image.png" alt='Imagem do Hero'/>
+                <img src="/Hero-image.png" alt='Imagem do Hero' />
+            </section>
+            <section>
+                <Social />
             </section>
         </>
     )
